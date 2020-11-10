473283c76485063c68ca9477f144abdbe3948180
Takahiro <t.shinkai00@gmail.com>
2020-11-10 16:40:22 +0900

    add handmadePlayer file

--- Player/all.hpp
+++ Player/all.hpp
@@ -1,6 +1,6 @@
 #include "chototsuPlayer.hpp"
 #include "humanPlayer.hpp"
 #include "randomPlayer.hpp"
-#include "ChototsuPlayer2.hpp"
+#include "handmadePlayer.hpp"
 #include "MonteCarloPlayer.hpp"
 #include "randomPlayer2.hpp"
