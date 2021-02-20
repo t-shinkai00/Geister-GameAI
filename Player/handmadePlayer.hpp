6a5fef01f300786497fcbe72e335c08d9f905299
t-shinkai00 <t.shinkai00@gmail.com>
2021-02-20 23:57:54 +0900

    add takeRed and takeBlue count

--- Player/handmadePlayer.hpp
+++ Player/handmadePlayer.hpp
@@ -19,7 +19,12 @@ public:
     {
         return std::string("ADFG");
     }
+    virtual std::vector<Hand> candidateHand()
+    {
+        return game.getLegalMove1st();
+    }
 
+    //現在何手目かの変数
     int present = 0;
 
     virtual std::string decideHand(std::string res)
@@ -28,20 +33,8 @@ public:
         game.setState(res);
         const std::array<Unit, 16> &units = game.allUnit();
         auto legalMoves = candidateHand();
+        std::uniform_int_distribution<int> serector1(0, legalMoves.size() - 1);
 
-        int takeRed = 0;
-        int takeBlue = 0;
-        for (int u = 8; u < 16; ++u)
-        {
-            const Unit &unit = units[u];
-            if (unit.x() == 9 && unit.y() == 9)
-            {
-                if (unit.color() == UnitColor::Red)
-                    takeRed++;
-                if (unit.color() == UnitColor::Blue)
-                    takeBlue++;
-            }
-        }
         // ゴール
         for (const Unit &u : units)
         {
@@ -58,10 +51,25 @@ public:
             }
         }
 
+        //取得した赤コマと青コマの数
+        int takeRed = 0;
+        int takeBlue = 0;
+        for (int u = 8; u < 16; ++u)
+        {
+            const Unit &unit = units[u];
+            if (unit.x() == 9 && unit.y() == 9)
+            {
+                if (unit.color() == UnitColor::Red)
+                    takeRed++;
+                if (unit.color() == UnitColor::Blue)
+                    takeBlue++;
+            }
+        }
+
         //初期のコマの動き方
         if (present == 1)
         {
-            Hand action = Hand({units[4], Direction::West});
+            auto action = Hand({units[4], Direction::West});
             return action;
         }
         else if (present == 2)
@@ -77,9 +85,11 @@ public:
             return Hand({units[6], Direction::East});
         }
 
+        //相手の赤コマが近づいた場合
         for (int u = 8; u < 16; ++u)
         {
-            if (takeRed == 3)
+            //取った赤コマが既に3の場合
+            if (takeRed == 3 && takeBlue < 3)
                 break;
             const Unit &unit = units[u];
             if (unit.x() == units[4].x() + 1 && unit.y() == units[4].y() && units[4].y() > 3 && units[4].y() < 6)
@@ -190,62 +200,6 @@ public:
             {
                 return Hand({units[6], Direction::North});
             }
-            // }else return Hand({units[6],Direction::South});
-
-            // if(unit.color() == UnitColor::unknown && units[4].x()==0 && units[4].y()==5){
-            //     if(unit.x() == 0 && unit.y() == 4){
-            //         return Hand({units[4],Direction::North});
-            //     }else if(unit.x() == 1 && unit.y() == 5){
-            //         return Hand({units[4],Direction::East});
-            //     }
-            // }
-            // if(unit.color() == UnitColor::unknown && units[0].x()==1 && units[0].y()==4){
-            //     if(unit.x() == 0 && unit.y() == 4){
-            //         return Hand({units[0],Direction::West});
-            //     }else if(unit.x() == 1 && unit.y() == 3){
-            //         return Hand({units[0],Direction::North});
-            //     }else if(unit.x() == 2 && unit.y() == 4){
-            //         return Hand({units[0],Direction::East});
-            //     }else if(unit.x() == 1 && unit.y() == 5){
-            //         return Hand({units[0],Direction::South});
-            //     }
-            // }
-            // if(unit.color() == UnitColor::unknown && units[5].x()==1 && units[5].y()==5){
-            //     if(unit.x() == 0 && unit.y() == 5){
-            //         return Hand({units[5],Direction::West});
-            //     }else if(unit.x() == 1 && unit.y() == 4){
-            //         return Hand({units[5],Direction::North});
-            //     }else if(unit.x() == 2 && unit.y() == 5){
-            //         return Hand({units[5],Direction::East});
-            //     }
-            // }
-            // if(unit.color() == UnitColor::unknown && units[7].x()==5 && units[7].y()==5){
-            //     if(unit.x() == 5 && unit.y() == 4){
-            //         return Hand({units[7],Direction::North});
-            //     }else if(unit.x() == 4 && unit.y() == 5){
-            //         return Hand({units[7],Direction::East});
-            //     }
-            // }
-            // if(unit.color() == UnitColor::unknown && units[3].x()==4 && units[3].y()==4){
-            //     if(unit.x() == 3 && unit.y() == 4){
-            //         return Hand({units[3],Direction::West});
-            //     }else if(unit.x() == 4 && unit.y() == 3){
-            //         return Hand({units[3],Direction::North});
-            //     }else if(unit.x() == 5 && unit.y() == 4){
-            //         return Hand({units[3],Direction::East});
-            //     }else if(unit.x() == 4 && unit.y() == 5){
-            //         return Hand({units[3],Direction::South});
-            //     }
-            // }
-            // if(unit.color() == UnitColor::unknown && units[6].x()==4 && units[6].y()==5){
-            //     if(unit.x() == 3 && unit.y() == 5){
-            //         return Hand({units[6],Direction::West});
-            //     }else if(unit.x() == 4 && unit.y() == 4){
-            //         return Hand({units[6],Direction::North});
-            //     }else if(unit.x() == 5 && unit.y() == 5){
-            //         return Hand({units[6],Direction::East});
-            //     }
-            // }
         }
 
         int mostFrontPos = units[0].y();
@@ -264,28 +218,28 @@ public:
         }
         if (units[mostFrontIndex].x() == 0 || units[mostFrontIndex].x() == 5)
         {
-            Hand action = Hand({units[mostFrontIndex], Direction::North});
+            auto action = Hand({units[mostFrontIndex], Direction::North});
             for (int i = 0; i < legalMoves.size(); i++)
                 if (action == legalMoves[i])
                     return action;
         }
         else if (units[mostFrontIndex].y() == 4 || units[mostFrontIndex].y() == 5)
         {
-            Hand action = Hand({units[mostFrontIndex], Direction::North});
+            auto action = Hand({units[mostFrontIndex], Direction::North});
             for (int i = 0; i < legalMoves.size(); i++)
                 if (action == legalMoves[i])
                     return action;
         }
         else if ((units[mostFrontIndex].x() == 1 || units[mostFrontIndex].x() == 2) && units[mostFrontIndex].y() <= 3 && units[mostFrontIndex].y() >= 0)
         {
-            Hand action = Hand({units[mostFrontIndex], Direction::West});
+            auto action = Hand({units[mostFrontIndex], Direction::West});
             for (int i = 0; i < legalMoves.size(); i++)
                 if (action == legalMoves[i])
                     return action;
         }
         else if ((units[mostFrontIndex].x() == 3 || units[mostFrontIndex].x() == 4) && units[mostFrontIndex].y() <= 3 && units[mostFrontIndex].y() >= 0)
         {
-            Hand action = Hand({units[mostFrontIndex], Direction::East});
+            auto action = Hand({units[mostFrontIndex], Direction::East});
             for (int i = 0; i < legalMoves.size(); i++)
                 if (action == legalMoves[i])
                     return action;
